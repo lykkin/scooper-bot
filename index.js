b0237936c4d4550d0594ce1071e8949c43d33e7b
bryan <bclement01@gmail.com>
2020-01-18 22:47:44 -0800

    uploading and cleaning

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const axios = require('axios')
 const Jimp = require('jimp')
 const FormData = require('form-data')
 const TelegramBot = require('node-telegram-bot-api')
-const TELEGRAM_API_PREFIX = 'https://api.telegram.org/bot' + process.env.TELEGRAM_TOKEN
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN)
+
+// number of stickers per set
+const SET_SIZE = 100
+
 
 const imageRegex = /jpg-large$|png-large$|\.png$|\.jpg$|\.jpeg$/i
-let botInfo
 
 function sleep(n) {
   return new Promise((resolve, reject) => {
@@ -15,44 +18,91 @@ function sleep(n) {
   })
 }
 
-async function getBumps() {
+function getStickerSetName({botInfo, setIdx}) {
+  return `shithouse_scoop_${setIdx}_by_${botInfo.username}`
+}
+
+async function uploadBumps({botInfo}) {
+  const sent = []
   const bumpRequest = axios.get('http://api.shithouse.tv')
-  botInfo = (await axios.get(`${TELEGRAM_API_PREFIX}/getMe`)).data.result
   const bumps = (await bumpRequest).data.filter(b => b.image && imageRegex.exec(b.image))
 
   for (let i = 0; i < bumps.length; ++i) {
     const bump = bumps[i]
-    const packIdx = (i / 200) | 0
+
+    // compute sticker set name
+    const setIdx = (i / SET_SIZE) | 0
+    const setName = getStickerSetName({botInfo, setIdx})
+
+    // compute image buffer to send
+    const im = await Jimp.read(`http://${bump.name}.shithouse.tv/${bump.image}`)
+    im.scaleToFit(512, 512)
+    const toSend = await im.getBufferAsync('image/png')
+
     try {
-    const packReq = await axios.post(TELEGRAM_API_PREFIX + '/createNewStickerSet', {
-      user_id: botInfo.id,
-      name: `shithouse_scoop_${packIdx}_by_${botInfo.username}`,
-      title: `poop scoop ${packIdx}`,
-      emojis: 'eggplant',
-      png_sticker: Buffer.from('')
-    })
-    console.log(packReq)
+      await bot.createNewStickerSet(
+        process.env.TELEGRAM_USER_ID,
+        setName,
+        `poop scoop ${setIdx}`,
+        toSend,
+        '👍'
+      )
+    } catch (e) {
+      if (e.response.body.description !== 'Bad Request: sticker set name is already occupied') {
+        console.log('error making set:', e)
+      } else {
+        console.log('ok')
+      }
+    }
+
+    try {
+      await bot.addStickerToSet(
+        process.env.TELEGRAM_USER_ID,
+        setName,
+        toSend,
+        "🍆"
+      )
     } catch (e) {
       console.log(e)
     }
-    const im = await Jimp.read(`http://${bump.name}.shithouse.tv/${bump.image}`)
-    const bitmap = im.bitmap
-    console.log('before', bitmap, bitmap.data.length/1024)
-    if (bitmap.height > bitmap.width) {
-      if (bitmap.height != 512) {
-        im.resize(Jimp.AUTO, 512)
-      }
-    } else {
-      if (bitmap.width != 512) {
-        im.resize(512, Jimp.AUTO)
-      }
-    }
-    console.log('after', im.bitmap, bitmap.data.length/1024)
-    if (bitmap.data.length/1024 > 512) {
-      //console.log(bitmap.data.length / 1024)
+    if (!sent[setIdx]) {
+      const stickerSet = await bot.getStickerSet(setName)
+      await bot.sendSticker(
+        process.env.TELEGRAM_USER_ID,
+        stickerSet.stickers[0].file_id
+      )
+      sent[setIdx] = true
     }
     await sleep(100)
   }
 }
 
-getBumps()
+async function getSet(setName) {
+  try {
+    return await bot.getStickerSet(setName)
+  } catch (e) {
+    return null
+  }
+}
+
+async function cleanBumps({botInfo}) {
+  let setIdx = 0
+  let currSet
+  while (currSet = await getSet(getStickerSetName({botInfo, setIdx}))) {
+    for (let s of currSet.stickers) {
+      await bot.deleteStickerFromSet(s.file_id)
+      await sleep(100)
+    }
+    ++setIdx
+  }
+}
+
+async function getMetadata() {
+  return {
+    botInfo: await bot.getMe()
+  }
+}
+
+getMetadata()
+  //.then(uploadBumps)
+  .then(cleanBumps)
