7a4a6807d84b03dfada79b2da53a3c5e4d5e8202
bryan <bclement01@gmail.com>
2020-01-20 14:13:56 -0800

    parallel uploads

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const bot = new TelegramBot(process.env.TELEGRAM_TOKEN)
 // number of stickers per set
 const SET_SIZE = 100
 
+// uploading is dominated by network requests, so we can push this up to
+// saturate the cpu with image processing
+const PARALLEL_UPLOADERS = 2
+
 
 const imageRegex = /jpg-large$|png-large$|\.png$|\.jpg$|\.jpeg$/i
 
@@ -18,17 +22,18 @@ function sleep(n) {
   })
 }
 
+function getEmoji() {
+  return '🍆'
+}
+
 function getStickerSetName({botInfo, setIdx}) {
   return `shithouse_scoop_${setIdx}_by_${botInfo.username}`
 }
 
-async function uploadBumps({botInfo}) {
-  const sent = []
-  const bumpRequest = axios.get('http://api.shithouse.tv')
-  const bumps = (await bumpRequest).data.filter(b => b.image && imageRegex.exec(b.image))
-
-  for (let i = 0; i < bumps.length; ++i) {
-    const bump = bumps[i]
+async function uploader(botInfo, bumps, sent) {
+  while(bumps.length) {
+    const bump = bumps.pop()
+    const i = bumps.length
 
     // compute sticker set name
     const setIdx = (i / SET_SIZE) | 0
@@ -38,6 +43,7 @@ async function uploadBumps({botInfo}) {
     const im = await Jimp.read(`http://${bump.name}.shithouse.tv/${bump.image}`)
     im.scaleToFit(512, 512)
     const toSend = await im.getBufferAsync('image/png')
+    console.log('processing', i, setIdx, bump)
 
     try {
       await bot.createNewStickerSet(
@@ -45,7 +51,7 @@ async function uploadBumps({botInfo}) {
         setName,
         `poop scoop ${setIdx}`,
         toSend,
-        '👍'
+        getEmoji()
       )
     } catch (e) {
       if (e.response.body.description !== 'Bad Request: sticker set name is already occupied') {
@@ -60,7 +66,7 @@ async function uploadBumps({botInfo}) {
         process.env.TELEGRAM_USER_ID,
         setName,
         toSend,
-        "🍆"
+        getEmoji()
       )
     } catch (e) {
       console.log(e)
@@ -77,6 +83,21 @@ async function uploadBumps({botInfo}) {
   }
 }
 
+function generateUploadBumps(numWorkers) {
+  const sent = []
+  return async function uploadBumps([{botInfo}, bumps]) {
+    for (let i = 0; i < numWorkers; ++i) {
+      uploader(botInfo, bumps, sent)
+    }
+  }
+}
+
+async function getBumps() {
+  const bumpRequest = axios.get('http://api.shithouse.tv')
+  return (await bumpRequest).data.filter(b => b.image && imageRegex.exec(b.image))
+
+}
+
 async function getSet(setName) {
   try {
     return await bot.getStickerSet(setName)
@@ -91,7 +112,6 @@ async function cleanBumps({botInfo}) {
   while (currSet = await getSet(getStickerSetName({botInfo, setIdx}))) {
     for (let s of currSet.stickers) {
       await bot.deleteStickerFromSet(s.file_id)
-      await sleep(100)
     }
     ++setIdx
   }
@@ -103,6 +123,9 @@ async function getMetadata() {
   }
 }
 
-getMetadata()
-  //.then(uploadBumps)
-  .then(cleanBumps)
+Promise.all([
+  getMetadata(),
+  getBumps()
+])
+  .then(generateUploadBumps(PARALLEL_UPLOADERS))
+  //.then(cleanBumps)
