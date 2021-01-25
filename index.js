206912fb7cc9f3b39065b9f8479d5ca432035330
David Meade <dman757@gmail.com>
2021-01-25 14:35:38 -0800

    STARTING SWOLE BOT

--- index.js
+++ index.js
@@ -212,6 +212,23 @@ async function getMetadata() {
   }
 }
 
+bot.onText(
+  /(?<lift>[a-zA-Z]+): (?<sets>[0-9]+)x(?<reps>[0-9]+)@(?<weight>[0-9]+)/,
+  (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+    const chatId = msg.chat.id;
+    // const resp = match[1]; // the captured "whatever"
+    const response = `YA GONNA GET SWOLE DOING ${match[1].toUpperCase()}?
+  SETS: ${match[2]}
+  REPS: ${match[3]}
+  WEIGHT: ${match[4]}`;
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, response);
+  }
+);
+
 Promise.all([
   getMetadata(),
   getDbFileHandle().then(loadDb),
